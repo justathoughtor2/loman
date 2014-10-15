f0d2df21a6d5843cf272bba2d9cea50b3ea986dc
justathoughtor2 <thoughthaven@gmail.com>
2014-10-15 11:27:10 -0400

    Add header and footer

---
+++ footer.php
@@ -0,0 +1,10 @@
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<script src="js/boostrap.min.js"></script>
+				</div>
+			</div>
+		</div>
+
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title><?php wp_title( '|', true, 'right' ); ?></title>
+		<?php wp_head(); ?>
+	</head>
+	
+	<body <?php body_class(); ?>>
